c470100fb9300cc493a77400cd6c5bcde3d468d7
JessicaroseD1 <>
2023-08-16 09:46:54 +0000

    Updated startGame function so start button would function

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
-let startButton = document.getElementById("start-btn")
-startButton.addEventListener("click", startGame)
-console.log("Started"); // This is not appearing in the console
+let startButton = document.getElementById("start-btn");
+startButton.addEventListener("click", startGame);
+// This is not appearing in the console
 
 function startGame() {
-  
+    console.log("Started");
 }
 
 function setNextQuestion() {
@@ -16,9 +16,11 @@ function selectAnswer() {
 
 // Questions that will be asked
 
-    const questions = [ {
+const questions = [
+    {
         question: 'In The Matrix, does Neo take the blue pill or the red pill?',
-            answers: [{
+        answers: [
+            {
                 text: 'red',
                 correct: true
             },
@@ -29,73 +31,72 @@ function selectAnswer() {
             {
                 text: 'green',
                 correct: false
-            }
+            },
             {
                 text: 'orange',
                 correct: false
-                
+
             }
-            ]
-        },
-        {
+        ]
+    },
+    {
         question: 'In the film Mean Girls, what day do they wear pink?',
-            answers: [{
-                text: 'wednesday',
-                correct: true
-            },
-            {
-                text: 'monday',
-                correct: false
-            },
-            {
-                text: 'thursday',
-                correct: false
-            },
-            {
-                text: 'friday',
-                correct: false
-            }
-            ]
+        answers: [{
+            text: 'wednesday',
+            correct: true
         },
         {
-        question: 'What DC movie was shockingly canceled by Warner Bros. in 2022?',
-            answers: [{
-                text: 'batgirl',
-                correct: true
-            },
-           {
-                text: 'batman',
-                correct: false
-            }, 
-            {
-                text: 'batman',
-                correct: false
-            },
-            {
-                text: 'venom',
-                 correct: false
-                },
-            ]
+            text: 'monday',
+            correct: false
         },
         {
-        question: 'What is the first rule of Fight Club?',
-            answers: [{
-                text: 'You do not talk about Fight Club.',
-                correct: true
-            },
-            {
-                text: 'have a glass of water',
-                    correct: false
-                },
-            {
-                text: 'eat nothing but chicken',
-                correct: false
-            },
-            {
-                text: 'sleep for 3 hours before the fight',
-                correct: false
-            },
+            text: 'thursday',
+            correct: false
+        },
+        {
+            text: 'friday',
+            correct: false
         }
         ]
-        
-
+    },
+    {
+        question: 'What DC movie was shockingly canceled by Warner Bros. in 2022?',
+        answers: [{
+            text: 'batgirl',
+            correct: true
+        },
+        {
+            text: 'batman',
+            correct: false
+        },
+        {
+            text: 'batman',
+            correct: false
+        },
+        {
+            text: 'venom',
+            correct: false
+        },
+        ]
+    },
+    {
+        question: 'What is the first rule of Fight Club?',
+        answers: [{
+            text: 'You do not talk about Fight Club.',
+            correct: true
+        },
+        {
+            text: 'have a glass of water',
+            correct: false
+        },
+        {
+            text: 'eat nothing but chicken',
+            correct: false
+        },
+        {
+            text: 'sleep for 3 hours before the fight',
+            correct: false
+        }
+        ]
+    }
+];
