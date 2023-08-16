152cc303aad7a5a871f249f2a76389f1e34fa328
JessicaroseD1 <>
2023-08-16 09:27:40 +0000

    Add an Event Listener to the start button

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,7 +1,9 @@
-const startButton = document.getElementById('start-btn');
+let startButton = document.getElementById("start-btn")
+startButton.addEventListener("click", startGame)
+console.log("Started"); // This is not appearing in the console
 
 function startGame() {
-
+  
 }
 
 function setNextQuestion() {
@@ -12,6 +14,88 @@ function selectAnswer() {
 
 }
 
+// Questions that will be asked
 
-
+    const questions = [ {
+        question: 'In The Matrix, does Neo take the blue pill or the red pill?',
+            answers: [{
+                text: 'red',
+                correct: true
+            },
+            {
+                text: 'blue',
+                correct: false
+            },
+            {
+                text: 'green',
+                correct: false
+            }
+            {
+                text: 'orange',
+                correct: false
+                
+            }
+            ]
+        },
+        {
+        question: 'In the film Mean Girls, what day do they wear pink?',
+            answers: [{
+                text: 'wednesday',
+                correct: true
+            },
+            {
+                text: 'monday',
+                correct: false
+            },
+            {
+                text: 'thursday',
+                correct: false
+            },
+            {
+                text: 'friday',
+                correct: false
+            }
+            ]
+        },
+        {
+        question: 'What DC movie was shockingly canceled by Warner Bros. in 2022?',
+            answers: [{
+                text: 'batgirl',
+                correct: true
+            },
+           {
+                text: 'batman',
+                correct: false
+            }, 
+            {
+                text: 'batman',
+                correct: false
+            },
+            {
+                text: 'venom',
+                 correct: false
+                },
+            ]
+        },
+        {
+        question: 'What is the first rule of Fight Club?',
+            answers: [{
+                text: 'You do not talk about Fight Club.',
+                correct: true
+            },
+            {
+                text: 'have a glass of water',
+                    correct: false
+                },
+            {
+                text: 'eat nothing but chicken',
+                correct: false
+            },
+            {
+                text: 'sleep for 3 hours before the fight',
+                correct: false
+            },
+        }
+        ]
+        
 
