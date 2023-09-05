feaea5ad3eb0e2fb51d1ffd9b589396dbecc746c
JessicaroseD1 <>
2023-09-05 05:49:21 +0000

    Update

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,110 +1,130 @@
-const startButton = document.getElementById("start-btn");
-startButton.addEventListener("click", startGame);
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const endButton = document.getElementById('end-btn');
 const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
+const introText = document.getElementById('intro-text');
 
 let shuffledQuestions, currentQuestionIndex;
 
-function startGame() {
-    console.log("Started");
+startButton.addEventListener('click', main);
+endButton.addEventListener('click', endGame);
+nextButton.addEventListener('click', () => {
+    currentQuestionIndex++;
+    setNextQuestion();
+});
+
+// need to add a function to remove the text ()remove child possibly? OR can I add it to the main function?
+
+// either I use a const to call element by ID which I have given in <p> in the html tag
+
+// add it as a const? with a remove.element as well?
+
+function main() {
     startButton.classList.add('hide');
+    endButton.classList.add('hide');
+    shuffledQuestions = questions.sort(() => Math.random() - .5);
+    currentQuestionIndex = 0;
     questionContainerElement.classList.remove('hide');
+    //introText.classList.add('hide'); 
+
     setNextQuestion();
 }
 
+function endGame() {
+    rem;
+}
+
+
+// Add in an end game button maybe? Also a results section
+// capture the right answers in a variable 
 function setNextQuestion() {
-
+    resetState();
+    showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
-function selectAnswer() {
-
+function showQuestion(question) {
+    questionElement.innerText = question.question;
+    question.answers.forEach(answer => {
+        const button = document.createElement('button');
+        button.innerText = answer.text;
+        button.classList.add('btn');
+        if (answer.correct) {
+            button.dataset.correct = answer.correct;
+        }
+        button.addEventListener('click', selectAnswer);
+        answerButtonsElement.appendChild(button);
+    });
 }
 
-// Questions that will be asked
+function resetState() {
+    clearStatusClass(document.body);
+    nextButton.classList.add('hide');
+    while (answerButtonsElement.firstChild) {
+        answerButtonsElement.removeChild(answerButtonsElement.firstChild);
+    }
+}
+
+function selectAnswer(e) {
+    const selectedButton = e.target;
+    const correct = selectedButton.dataset.correct;
+    setStatusClass(document.body, correct);
+    Array.from(answerButtonsElement.children).forEach(button => {
+        setStatusClass(button, button.dataset.correct);
+    });
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+        nextButton.classList.remove('hide');
+    } else {
+        startButton.innerText = 'Play Again';
+        startButton.classList.remove('hide');
+        endButton.classList.remove('hide');
+    }
+}
+
+function setStatusClass(element, correct) {
+    clearStatusClass(element);
+    if (correct) {
+        element.classList.add('correct');
+    } else {
+        element.classList.add('wrong');
+    }
+}
+
+function clearStatusClass(element) {
+    element.classList.remove('correct');
+    element.classList.remove('wrong');
+}
 
 const questions = [
     {
-        question: 'In The Matrix, does Neo take the blue pill or the red pill?',
+        question: 'In The Matrix, does Neo take the blue pill or the red pill',
         answers: [
-            {
-                text: 'red',
-                correct: true
-            },
-            {
-                text: 'blue',
-                correct: false
-            },
-            {
-                text: 'green',
-                correct: false
-            },
-            {
-                text: 'orange',
-                correct: false
-
-            }
+            { text: 'red', correct: true },
+            { text: 'blue', correct: false }
         ]
     },
     {
         question: 'In the film Mean Girls, what day do they wear pink?',
         answers: [
-            {
-                text: 'wednesday',
-                correct: true
-            },
-            {
-                text: 'monday',
-                correct: false
-            },
-            {
-                text: 'thursday',
-                correct: false
-            },
-            {
-                text: 'friday',
-                correct: false
-            }
+            { text: 'Wednesday', correct: true },
+            { text: 'Thursday', correct: false },
         ]
     },
     {
         question: 'What DC movie was shockingly canceled by Warner Bros. in 2022?',
         answers: [
-            {
-                text: 'batgirl',
-                correct: true
-            },
-            {
-                text: 'batman',
-                correct: false
-            },
-            {
-                text: 'batman',
-                correct: false
-            },
-            {
-                text: 'venom',
-                correct: false
-            },
+            { text: 'batgirl', correct: true },
+            { text: 'batman', correct: false },
         ]
     },
     {
-        question: 'What is the first rule of Fight Club?',
+        question: 'What is the first rule of Fight Club??',
         answers: [
-            {
-                text: 'You do not talk about Fight Club.',
-                correct: true
-            },
-            {
-                text: 'have a glass of water',
-                correct: false
-            },
-            {
-                text: 'eat nothing but chicken',
-                correct: false
-            },
-            {
-                text: 'sleep for 3 hours before the fight',
-                correct: false
-            }
+            { text: 'eat nothing but chicken', correct: false },
+            { text: 'You do not talk about Fight Club.', correct: true },
         ]
     }
 ];
+
+// Add in more questions
